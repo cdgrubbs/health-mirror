459f425466f3b6b1651b71e271bf92b32d4a55c2
internuser1 <zhanghow@umich.edu>
2020-04-24 12:51:15 -0400

    main screen layout and trigger

--- mirror/widgets/listener.py
+++ mirror/widgets/listener.py
@@ -18,7 +18,7 @@ FORMAT = pyaudio.paInt16
 CHANNELS = 2
 RATE = 44100
 RECORD_SECONDS = 5
-WAVE_OUTPUT_FILENAME = "output.wav"
+WAVE_OUTPUT_FILENAME = "audio_recording.wav"
 TELL_JOKE = [
     "tell me a joke",
     "tell a joke",
@@ -66,68 +66,46 @@ JOURNAL = [
     "i want to add to my gratitude journal"
 ]
 
-AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "../../output.wav")
+AUDIO_FILE = path.join(path.dirname(path.realpath(__file__)), "../../audio_recording.wav")
 
 class Listener(QWidget):
     def __init__(self, parent):
         super(Listener, self).__init__()
+        self.journal_entries = []
+        # self.record_audio()
+        # time.sleep(5)
+        # self.get_results()
+        # self.setParent(parent)
+
+    def doListener(self):
         self.record_audio()
         time.sleep(5)
-        self.get_results()
-        self.setParent(parent)
-        self.journal_entries = []
+        return self.get_results()
 
     def record_audio(self):
         p = pyaudio.PyAudio()
-
         stream = p.open(format=FORMAT,
                         channels=CHANNELS,
                         rate=RATE,
                         input=True,
                         frames_per_buffer=CHUNK)
-
         print("* recording")
-
         frames = []
-
         for i in range(0, int(RATE / CHUNK * RECORD_SECONDS)):
             data = stream.read(CHUNK)
             frames.append(data)
-
         print("* done recording")
-
         stream.stop_stream()
         stream.close()
         p.terminate()
 
-        wf = wave.open(WAVE_OUTPUT_FILENAME, 'wb')
+        OUTPUT_FILE = path.join(path.dirname(path.realpath(__file__)), "../../audio_recording.wav")
+        wf = wave.open(OUTPUT_FILE, 'wb')
         wf.setnchannels(CHANNELS)
         wf.setsampwidth(p.get_sample_size(FORMAT))
         wf.setframerate(RATE)
         wf.writeframes(b''.join(frames))
         wf.close()
-    
-    def record_and_parse_audio(self):
-        while True:
-            self.record_audio()
-
-            r = sr.Recognizer()
-            with sr.AudioFile(AUDIO_FILE) as source:
-                audio = r.record(source)
-
-            try:
-                print("You said:")
-                print(r.recognize_google(audio))
-                wordString = r.recognize_google(audio)
-                return wordString
-
-            except sr.UnknownValueError:
-                print("Google Speech Recognition could not understand audio")
-                print("Please try again")
-
-            except sr.RequestError as e:
-                print("Could not request results from Google Speech Recognition service; {0}".format(e))
-                print("Please try again")
 
     def get_results(self):
         # use the audio file as the audio source
@@ -152,87 +130,55 @@ class Listener(QWidget):
             print("Could not request results from Google Speech Recognition service; {0}".format(e))
             return
 
-        
-
+        print('searching for widget')
+        widgetName = 'nothing found'
         for utterance in TELL_JOKE:
             if utterance.lower() == wordString.lower():
-                self.joke()
+                widgetName = 'joke'
 
         for utterance in BREATHING:
             if utterance.lower() == wordString.lower():
-                self.breathe()
+                widgetName = 'breathe'
 
         for utterance in MEDICATION:
             if utterance.lower() == wordString.lower():
-                self.medication()
-
-        for utterance in LIGHTS_ON:
-            if utterance.lower() == wordString.lower():
-                self.light_on()
-
-        for utterance in LIGHTS_OFF:
-            if utterance.lower() == wordString.lower():
-                self.light_off()
+                widgetName = 'reminder'
 
         for utterance in TALK:
             if utterance.lower() == wordString.lower():
-                self.talk()
+                widgetName = 'talk'
 
         for utterance in JOURNAL:
             if utterance.lower() == wordString.lower():
-                self.journal()
+                widgetName = 'journal'
 
-    def joke(self):
-        jokes = [
-            "What do you call a computer floating in the ocean? A Dell Rolling in the Deep",
-            "There are 10 types of people in the world: those who understand binary, and those who don’t",
-            "An SQL statement walks into a bar and sees two tables. It approaches, and asks “may I join you?”",
-            "Q: How many programmers does it take to change a light bulb? A: None. It’s a hardware problem.",
-            "The programmer got stuck in the shower because the instructions on the shampoo bottle said: Lather, Rinse, Repeat",
-            "A programmer’s wife tells him, “While you’re at the grocery store, buy some eggs.” He never comes back"
-        ]
-        index = random.randint(0,5)
-        self.label = QLabel(jokes[index])
-        self.label.setParent(self)
-        self.label.setWordWrap(True)
-        self.label.setStyleSheet('color: white; font-size: 18px')
-    
-    def breathe(self):
-        print("Beginning breathing exercises")
-        url = "https://www.youtube.com/watch?v=5DqTuWve9t8"
-        #Display youtube video
+        return widgetName
 
-    def medication(self):
-        print("Okay, what time would you like to set the medication for?")
-        # Listen again for time
-        print("Okay, how often do you want the medication reminder?")
-        # Listen again for frequency
-        # Set reminder
+    # I think we can delete this?
+    def record_and_parse_audio(self):
+        while True:
+            self.record_audio()
 
-    def light_on(self):
-        print("Okay, turning on lights")
-        # Turn on lights
+            r = sr.Recognizer()
+            with sr.AudioFile(AUDIO_FILE) as source:
+                audio = r.record(source)
 
-    def light_off(self):
-        print("Okay, turning off lights")
-        # Turn off lights
+            try:
+                print("You said:")
+                print(r.recognize_google(audio))
+                wordString = r.recognize_google(audio)
+                return wordString
 
-    def talk(self):
-        print("Okay, what's up?")
-        # Record (maybe on a loop) and hit therapy api
-        
+            except sr.UnknownValueError:
+                print("Google Speech Recognition could not understand audio")
+                print("Please try again")
+
+            except sr.RequestError as e:
+                print("Could not request results from Google Speech Recognition service; {0}".format(e))
+                print("Please try again")
+
     def hide(self):
         self.label.hide()
 
     def show(self):
         self.label.show()
-
-    def journal(self):
-        numbers = ["first", "second", "third", "fourth", "fifth"]
-        print("Hello")
-        for number in numbers:
-            print("Say the {} thing you are grateful for".format(number))
-            word_string = self.record_and_parse_audio()
-            self.journal_entries.append(word_string)
-            if len(self.journal_entries) > 50:
-                journal_entries = journal_entries[1:]
